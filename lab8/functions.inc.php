25e734b11d92665caa87554911d68ad4ad3dee60
shizhangshishi <358024958@qq.com>
2019-05-22 23:29:07 +0800

    lab8 php

---
+++ lab8/functions.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+function outputOrderRow($file, $title, $quantity, $price) {
+
+    echo "<tr>";
+
+    echo '<td><img src="images/books/tinysquare/' . $file .'"></td>';
+
+    echo '<td class="mdl-data-table__cell--non-numeric">' . $title. '</td>';
+
+    echo '<td>'. $quantity .'</td>';
+
+    echo '<td>$' . number_format($price,2,'.','') .'</td>';
+
+    echo '<td>$'.   number_format($quantity * $price,2,'.','')  .'</td>';
+
+    echo "</tr>";
+
+}
+
+?>
